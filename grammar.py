cb04b1a77e49823a98d56b1699da20cbddc9cce0
Nic Cottrell <nic@cottrell.net>
2018-08-11 14:28:22 +0200

    Removed redundant functions in grammar.py

--- grammar.py
+++ grammar.py
@@ -1,49 +1,7 @@
-from numpy import argmax
-from keras.preprocessing.text import Tokenizer
-from keras.models import load_model
-
-from helpers import *
-
-
-# generate target given source sequence
-def predict_sequence(model, tokenizer, source):
-    """
-    :type tokenizer: Tokenizer
-    """
-    translations = list()
-    predictions = model.predict(source, verbose=0)
-    for p in predictions:
-        integers = [argmax(vector) for vector in p]
-        target = list()
-        for i in integers:
-            word = word_for_id(i, tokenizer)
-            if word is None:
-                break
-            target.append(word)
-        translations.append(' '.join(target))
-    print("Candidates=", translations)
-    return translations[0]
-
-
-def evaluate_model(model, tokenizer, sources, raw_dataset):
-    """
-    Evaluate a model on a small set of sentence pairs
-    :param model: The model to decode/infer with
-    :param tokenizer: The tokenizer used on the target language (to convert integer back to words)
-    :param sources: The encoded source sentences
-    :param raw_dataset: The original pairs to score against
-    """
-    actual, predicted = list(), list()
-    for i, source in enumerate(sources):
-        # translate encoded source text
-        source = source.reshape((1, source.shape[0]))
-        translation = predict_sequence(model, tokenizer, source)
-        raw_target, raw_src = raw_dataset[i]
-        if i < 20:
-            print('src=[%s], target=[%s], predicted=[%s]' % (raw_src, raw_target, translation))
-        actual.append(raw_target.split())
-        predicted.append(translation.split())
-
+"""
+Evaluate a small set of similar pairs for grammar handling
+"""
+from evaluate import *
 
 # load datasets
 dataset = load_clean_sentences('both')
