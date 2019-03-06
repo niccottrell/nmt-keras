8c9ef7e8af7edb7a952f0c4fa957a79abe201747
Nicholas Cottrell <nic@transmachina.com>
2019-03-06 10:55:37 +0100

    Added a histogram of sentence pair lengths for the report

---
+++ dataset.py
@@ -0,0 +1,27 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from helpers import load_clean_sentences
+
+# Load sentence lengths (characters)
+raw_dataset = load_clean_sentences()
+d = []
+for pair in raw_dataset:
+    d.append([len(pair[0]), len(pair[1])])
+
+langs = ['English', 'Swedish']
+data = pd.DataFrame(d, columns=langs)
+
+plt.figure()  # reset the plot
+for lang in langs:
+    # Draw the density plot
+    sns.distplot(data[lang], hist=False, kde=True,
+                 kde_kws={'shade': True, 'linewidth': 3},
+                 label=lang)
+
+# Plot formatting
+plt.legend(title='Sentence length')
+plt.xlabel('Sentence length (characters)')
+plt.ylabel('Density')
+# plt.show()
+plt.savefig("dataset.png")
